d1cdc0843ec2a56eefaf881a6d490e44ca9928f2
Marissa <marissaellingson@Marissas-MacBook.local>
2018-07-25 19:44:19 -0500

    file structure defined

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Regular Hamburger', false);
